1ac6ebdf19355a3eb4913c6ae3fd5ac19ee1559e
rajwant989 <rajwantsingh9892@gmail.com>
2025-08-01 18:49:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+  function turnOn() {
+    document.getElementById("img-1").src = "bulb-on.png";
+  }
+  function turnOff() {
+    document.getElementById("img-1").src = "bulb-off.png";
+  }
+
